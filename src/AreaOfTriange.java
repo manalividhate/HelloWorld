cb7b788555462670b253453a89856db9f09fd337
Manali vidhate <manali.vidhate@techdenovo.com>
2021-02-24 16:25:41 +0530

    refactor:devs:add sout statement

--- src/AreaOfTriange.java
+++ src/AreaOfTriange.java
@@ -4,16 +4,17 @@ public class AreaOfTriange {
 
             Scanner sc= new Scanner(System.in);
              public void calculation() {
-                 System.out.println("Enter width of Triangle:");
+                 System.out.println("here..we calculate Area of Triangle");
+                 System.out.println("Enter width ");
                  b = sc.nextDouble();
-                 System.out.println("Enter height of Triangle:");
+                 System.out.println("Enter height:");
                  h = sc.nextDouble();
                  area = (b * h) / 2;
                  System.out.println("Area of Triangle is: " + area);
              }
 
             public static void main(String args[])
-            {
+               {
                 AreaOfTriange areaOfTriange=new AreaOfTriange();
                 areaOfTriange.calculation();
 
